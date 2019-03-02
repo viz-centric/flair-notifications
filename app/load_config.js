9d9dc6fdd31bc321b7b45ca9dd477c91ebf685f6
admin-vizcentric <admin@vizcentric.com>
2019-03-02 14:21:51 +0000

    test correction

--- app/load_config.js
+++ app/load_config.js
@@ -1,12 +1,12 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
-var default_config='default_config.yml';
+var default_config='./app/default_config.yml';
 const configFile = process.env.APP_CONFIG || default_config;
 
 function load_appConfig(configFile){
     try {
         var AppConfig = yaml.safeLoad(fs.readFileSync(configFile, 'utf8'));
-    
+
         if (process.env.mailServiceAuthUser ){
             AppConfig.mailService.auth.user=process.env.mailServiceAuthUser;
         }
@@ -14,7 +14,7 @@ function load_appConfig(configFile){
             AppConfig.mailService.auth.pass=process.env.mailServiceAuthPass;
         }
         return AppConfig;
-    
+
     } catch (e) {
         console.log(e);
     }
