981f2e3ab4d53596c2d597aeb05d91134ac3492f
admin <admin@vizcentric.com>
2019-06-10 02:16:10 +0200

    completed report service

--- app/report/reportService.js
+++ app/report/reportService.js
@@ -3,15 +3,32 @@ const validator = require('./../validator');
 const logger = require('./../logger');
 
 
-module.exports ={
-    scheduleReport:scheduleReport
+module.exports = {
+    getScheduledReport: getScheduledReport,
+    scheduleReport: scheduleReport,
+    getAllScheduledReportForUser: getAllScheduledReportForUser,
+    getScheduledReportCountsForUser: getScheduledReportCountsForUser,
+    getScheduledReportLogs: getScheduledReportLogs,
+    updateScheduledReport: updateScheduledReport,
+    deleteScheduledReport: deleteScheduledReport,
+    executeReport: executeReport
 };
+
+/**
+ * message as a response for operations
+ * @param message
+ * @constructor
+ */
+function Message(message) {
+    this.message = message;
+}
+
 /**
  * Schedule a report.
  * @param report to be scheduled
- * @returns {Promise<any>}
+ * @returns {Promise<Message>}
  */
-function scheduleReport(report){
+function scheduleReport(report) {
     return new Promise(function (resolve, reject) {
         const result = validator.validateReportReqBody(report);
         if (result.error) {
@@ -20,30 +37,142 @@ function scheduleReport(report){
                 message: 'error in schedule api due to invalid request body',
                 errMsg: result.error.details[0].message
             });
-
-            reject({
-                message: result.error.details[0].message.replace(/"/g, "")
-            });
-        }
-        else {
+            reject(new Message(result.error.details[0].message.replace(/"/g, "")));
+        } else {
             jobs.createJob(report).then(function (result) {
                 if (result.success === 1) {
-                    resolve({
-                        message: result.message
-                    });
-                }
-                else {
-                    reject({
-                        message: result.message
-                    });
+                    resolve(new Message(result.message));
+                } else {
+                    reject(new Message(result.message));
                 }
             }, function (err) {
-                reject(err);
+                reject(new Message(err));
             });
         }
     });
 }
 
+/**
+ * Update the scheduled report
+ * @param report
+ * @return new Promise<Message>
+ */
+function updateScheduledReport(report) {
+    return new Promise(function (resolve, reject) {
+        const result = validator.validateReportReqBody(req.body);
+        if (result.error) {
+            resolve(new Message(result.error.details[0].message.replace(/"/g, "")))
+        } else {
+            jobs.modifyJob(req.body).then(function (result) {
+                if (result.success == 1) {
+                    resolve(new Message(result.message));
+                } else {
+                    reject(new Message(result.message));
+                }
+            }, function (err) {
+                reject(new Message(err));
+            })
+        }
+    });
+}
+
+/**
+ * Delete a scheduled report
+ * @param visualizationId
+ * @return {Promise<Message>}
+ */
+function deleteScheduledReport(visualizationId) {
+    return new Promise(function (resolve, reject) {
+        jobs.deleteJob(visualizationid).then(function (result) {
+            resolve(new Message(result));
+        }, function (err) {
+            reject(new Message(err))
+        })
+    })
+}
+
+/**
+ * Retrieve scheduled report by visualization id.
+ * @param visualizationId
+ * @return {Promise<Any>}
+ */
+function getScheduledReport(visualizationId) {
+    return new Promise(function (resolve, reject) {
+        jobs.getJob(visualizationid).then(function (result) {
+            if (result.message) {
+                resolve(new Message(result.message));
+            } else {
+                resolve(result);
+            }
+        }, function (err) {
+            reject(err);
+        })
+    })
+}
+
+/**
+ * Retrieve all scheduled reports for user paginated.
+ * @param username
+ * @param page which page
+ * @param size of the page
+ * @return {Promise<any>}
+ */
+function getAllScheduledReportForUser(username, page, size) {
+    return new Promise(function (resolve, reject) {
+        const page = (+page);
+        const pageSize = (+size);
+        jobs.JobsByUser(username, page, pageSize).then(function (result) {
+            resolve(result);
+        }, function (err) {
+            reject(err);
+        })
+    })
+}
+
+/**
+ * Retrieve scheduled report counts for the given user.
+ * @param username
+ * @return {Promise<any>}
+ */
+function getScheduledReportCountsForUser(username) {
+    return new Promise(function (resolve, reject) {
+        jobs.JobCountByUser(username).then(function (result) {
+            resolve(result);
+        }, function (err) {
+            reject(err);
+        })
+    })
+}
+
+/**
+ * Retrieve logs for the specified report.
+ * @param visualizationId
+ * @return {Promise<any>}
+ */
+function getScheduledReportLogs(visualizationId) {
+    return new Promise(function (resolve, reject) {
+        jobs.jobLogs(visualizationId).then(function (result) {
+            resolve(result);
+        }, function (err) {
+            reject(err);
+        })
+    })
+}
+
+/**
+ * Immediately execute a report.
+ * @param visualizationId
+ * @return {Promise<any>}
+ */
+function executeReport(visualizationId) {
+    return new Promise(function (resolve, reject) {
+        jobs.executeImmediate(visualizationId).then(function (result) {
+            resolve(result);
+        }, function (err) {
+            reject(err);
+        })
+    })
+}
 
 
 
