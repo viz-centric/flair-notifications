334dd8fdb782340e7d5a0fad8dae299003d3d838
johib.khan@gmail.com <johib.khan@gmail.com>
2019-08-06 18:00:03 +0530

    1)set visualization Id for threshold alert and normal report 2)pre-processor to set the viz id for threshold

--- app/database/DTOs/schedulerDTO.js
+++ app/database/DTOs/schedulerDTO.js
@@ -7,7 +7,7 @@ var queryDTO = require('./queryDTO');
 function scedulerDTO(schedulerObj){
     return {
       report:reportDTO(schedulerObj.userid,schedulerObj.dashboard_name,schedulerObj.view_name,schedulerObj.share_link,schedulerObj.build_url,
-      schedulerObj.mail_body,schedulerObj.subject,schedulerObj.report_name,schedulerObj.title_name,schedulerObj.createdAt), 
+      schedulerObj.mail_body,schedulerObj.subject,schedulerObj.report_name,schedulerObj.title_name,schedulerObj.createdAt,schedulerObj.thresholdAlert), 
       report_line_item:reportlineitemDTO(schedulerObj.reportline),
       assign_report:assignreportDTO(schedulerObj.AssignReport),
       schedule:schedulertaskDTO(schedulerObj.SchedulerTask),
