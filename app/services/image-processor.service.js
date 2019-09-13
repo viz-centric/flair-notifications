523180889960f231bcf1df81641a47e33bbf9f9f
johib.khan@gmail.com <johib.khan@gmail.com>
2019-09-13 15:39:13 +0530

    1)adding condition and logging in notification center if record is not found in engine 2)adding logging in image processsor

--- app/services/image-processor.service.js
+++ app/services/image-processor.service.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const AppConfig = require('../load_config');
 const base64Img = require('base64-img');
 const logger = require('./../logger');
+var models = require('../database/models/index');
 
 function createImageDir(config) {
   const imageDir = config.imageFolder;
@@ -32,12 +33,42 @@ function generateImage(svgHtml, image_dir, imageName) {
           }
           resolve(encodedUrl);
         }, function (error) {
+          logger.log({
+              level: 'error',
+              message: "error occured while converting image to base64 uri",
+              errMsg: "error occured while converting image to base64 uri : "+error.message,
+          });
+          let shedularlog = models.SchedulerTaskLog.create({
+              SchedulerJobId: reports_data['report_shedular_obj']['id'],
+              task_executed: new Date(Date.now()).toISOString(),
+              task_status: "error occured while converting image to base64 uri : "+error.message,
+          });
           reject(error.message);
         });
       }, function (error) {
+        logger.log({
+            level: 'error',
+            message: "error occured while converting svg html to image",
+            errMsg: "error occured while converting svg html to image : "+error.message,
+        });
+        let shedularlog = models.SchedulerTaskLog.create({
+            SchedulerJobId: reports_data['report_shedular_obj']['id'],
+            task_executed: new Date(Date.now()).toISOString(),
+            task_status: "error occured while converting svg html to image : "+error.message,
+        });
         reject(error.message);
       });
     } catch (ex) {
+      logger.log({
+          level: 'error',
+          message: "error occured while processing an image",
+          errMsg: "error occured while processing an image : "+ex.message,
+      });
+      let shedularlog = models.SchedulerTaskLog.create({
+          SchedulerJobId: reports_data['report_shedular_obj']['id'],
+          task_executed: new Date(Date.now()).toISOString(),
+          task_status: "error occured while processing an image"+ex.message,
+      });
       reject(ex.message);
     }
   });
