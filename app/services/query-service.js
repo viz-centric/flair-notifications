f728663a37bb71ec19576eebc0382d67ec40c4fc
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2020-03-15 17:53:01 +0200

    fixed date format in flair ql

--- app/services/query-service.js
+++ app/services/query-service.js
@@ -1,10 +1,11 @@
 const logger = require('./../logger');
+const moment = require('moment');
 
 function preProcessQuery(query) {
   logger.info(`Pre-processing query ${query}`);
   const stringQuery = JSON.stringify(query);
   const result = stringQuery.split('__FLAIR_NOW()')
-    .join("__FLAIR_CAST(timestamp, '" + new Date().toISOString() + "')");
+    .join("__FLAIR_CAST(timestamp, '" + moment().utc().format('YYYY-MM-DD HH:mm:ss') + "')");
   logger.info(`Pre-processing query result ${result}`);
   return JSON.parse(result);
 }
