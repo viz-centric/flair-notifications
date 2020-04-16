f3bf72ab2184f0b5d5a4c035fcd9918beb9925f6
Vizcentric Admin <admin@vizcentric.com>
2020-04-16 21:37:21 +0200

    Update release-snapshot.sh

--- release-snapshot.sh
+++ release-snapshot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd docker/dev
-docker-compose run --rm release npm run release -- patch --ci --preRelease=SNAPSHOT --no-git
+docker-compose run --rm release npm run release -- patch --ci --preRelease=SNAPSHOT --no-git.tag
