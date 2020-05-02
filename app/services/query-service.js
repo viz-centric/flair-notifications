d12f785a26f57bead9210a4e2dfb40dc9199f428
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2020-05-02 14:50:24 +0300

    added microseconds to query

--- app/services/query-service.js
+++ app/services/query-service.js
@@ -5,7 +5,7 @@ function preProcessQuery(query) {
   logger.info(`Pre-processing query ${query}`);
   const stringQuery = JSON.stringify(query);
   const result = stringQuery.split('__FLAIR_NOW()')
-    .join("__FLAIR_CAST(timestamp, '" + moment().utc().format('YYYY-MM-DD HH:mm:ss') + "')");
+    .join("__FLAIR_CAST(timestamp, '" + moment().utc().format('YYYY-MM-DD HH:mm:ss') + ".000000')");
   logger.info(`Pre-processing query result ${result}`);
   return JSON.parse(result);
 }
